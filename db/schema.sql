0f7a31a95fc589a5bc60786dfb39c397b9de5d3f
Taj English <tajenglish@gmail.com>
2017-02-16 19:37:53 -0500

    Added db schema and seed

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,10 @@ DROP TABLE IF EXISTS Configurations;
 CREATE TABLE Configurations(
   id SERIAL PRIMARY KEY,
   name varchar(250),
+  hostname varchar(250),
   port Integer,
   username varchar(250)
 );
+
+INSERT INTO Configurations (name, hostname, port, username)
+Values ('host1', 'nessus-ntp.lab.com', 1241, 'toto');
